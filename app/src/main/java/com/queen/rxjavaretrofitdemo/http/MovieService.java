415361f6d5ef5f708c5749c9834b5f8849373ba4
Zhongwy <805580462@qq.com>
2016-03-26 20:10:55 +0800

    update

--- app/src/main/java/com/queen/rxjavaretrofitdemo/http/MovieService.java
+++ app/src/main/java/com/queen/rxjavaretrofitdemo/http/MovieService.java
@@ -14,14 +14,7 @@ import rx.Observable;
  */
 public interface MovieService {
 
-//    @GET("top250")
-//    Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
 
-//    @GET("top250")
-//    Observable<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
-
-//    @GET("top250")
-//    Observable<HttpResult<List<Subject>>> getTopMovie(@Query("start") int start, @Query("count") int count);
 
     @GET("top250")
     Observable<HttpResult<List<Subject>>> getTopMovie(@Query("start") int start, @Query("count") int count);
